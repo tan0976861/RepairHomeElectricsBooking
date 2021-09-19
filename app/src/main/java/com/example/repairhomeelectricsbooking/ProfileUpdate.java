4665be1f0f254d7302e1678edbc27c3efe44b80d
Tran Cong Liem <liemtc16@gmail.com>
2021-09-19 14:15:53 +0700

    [LiemTC]_EditProfile_19092021

--- app/src/main/java/com/example/repairhomeelectricsbooking/ProfileUpdate.java
+++ app/src/main/java/com/example/repairhomeelectricsbooking/ProfileUpdate.java
@@ -2,20 +2,18 @@ package com.example.repairhomeelectricsbooking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Profile extends AppCompatActivity {
+public class ProfileUpdate extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_profile_update);
     }
 
-    public void clickToUpdateProfile(View view){
-        Intent intent= new Intent(this, ProfileUpdate.class);
-        startActivity(intent);
+    public void clickToBackProfile(View view){
+        finish();
     }
 }
