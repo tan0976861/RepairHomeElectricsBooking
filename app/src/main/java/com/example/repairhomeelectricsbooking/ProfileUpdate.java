907593dea0a4faa47210ca0a7c518094a14798fa
Tran Cong Liem <liemtc16@gmail.com>
2021-09-20 22:34:36 +0700

    [LiemTC]_EditInterFace_20092021

--- app/src/main/java/com/example/repairhomeelectricsbooking/ProfileUpdate.java
+++ app/src/main/java/com/example/repairhomeelectricsbooking/ProfileUpdate.java
@@ -3,15 +3,39 @@ package com.example.repairhomeelectricsbooking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
+import com.example.repairhomeelectricsbooking.R.id;
 
 public class ProfileUpdate extends AppCompatActivity {
 
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_update);
+
+        editText= findViewById(id.edtUsername);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
+
 
     public void clickToBackProfile(View view){
         finish();
