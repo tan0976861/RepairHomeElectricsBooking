b5ebd2f61d0d95499c3f5ac5628f911a0d1cac07
TNT <tan0976861@gmail.com>
2021-09-21 23:26:01 +0700

    [TanTN]_21092021

--- app/src/main/java/com/example/repairhomeelectricsbooking/MainActivity.java
+++ app/src/main/java/com/example/repairhomeelectricsbooking/MainActivity.java
@@ -1,27 +1,74 @@
 package com.example.repairhomeelectricsbooking;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
+import com.aurelhubert.ahbottomnavigation.notification.AHNotification;
 
+public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AHBottomNavigation bottomNavigation = (AHBottomNavigation) findViewById(R.id.bottom_navigation);
+
+// Create items
+        AHBottomNavigationItem item1 = new AHBottomNavigationItem(R.string.tab1, R.drawable.home, R.color.bluesky);
+        AHBottomNavigationItem item2 = new AHBottomNavigationItem(R.string.tab2, R.drawable.task, R.color.bluesky);
+        AHBottomNavigationItem item3 = new AHBottomNavigationItem(R.string.tab3, R.drawable.notification, R.color.bluesky);
+        AHBottomNavigationItem item4 = new AHBottomNavigationItem(R.string.tab4, R.drawable.info, R.color.bluesky);
+
+
+// Add items
+        bottomNavigation.addItem(item1);
+        bottomNavigation.addItem(item2);
+        bottomNavigation.addItem(item3);
+        bottomNavigation.addItem(item4);
+        bottomNavigation.setColored(false);
+        bottomNavigation.setDefaultBackgroundColor(getResources().getColor(R.color.white));
+        bottomNavigation.setAccentColor(getResources().getColor(R.color.bluesky));
+        bottomNavigation.setInactiveColor(getResources().getColor(R.color.bluesky));
+
+        AHNotification notification = new AHNotification.Builder()
+                .setText("10")
+                .setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.red))
+                .setTextColor(ContextCompat.getColor(MainActivity.this, R.color.white))
+                .build();
+        bottomNavigation.setNotification(notification, 2);
+
+        bottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
+            @Override
+            public boolean onTabSelected(int position, boolean wasSelected) {
+                switch (position){
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        Intent intent = new Intent(getApplicationContext(),ProfileUpdateActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        System.out.printf("tan");
+    public void setUpViewPaper(){
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(viewPagerAdapter);
     }
 
-    public void clickToViewInformation(View view){
-        Intent intent= new Intent(this, ProfileActivity.class);
-        startActivity(intent);
-    }
 }
