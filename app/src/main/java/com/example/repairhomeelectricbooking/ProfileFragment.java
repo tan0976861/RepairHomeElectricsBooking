a25c08395e9c8b138f45fee89132c150f7814c6a
TNT <tan0976861@gmail.com>
2021-09-29 17:34:42 +0700

    [TanTN]_29092021

--- app/src/main/java/com/example/repairhomeelectricbooking/ProfileFragment.java
+++ app/src/main/java/com/example/repairhomeelectricbooking/ProfileFragment.java
@@ -1,6 +1,8 @@
 package com.example.repairhomeelectricbooking;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,7 +10,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,20 +26,50 @@ import android.widget.TextView;
  */
 public class ProfileFragment extends Fragment {
 
-    private TextView tvChinhSuaTaiKhoan;
+    private LinearLayout layoutChinhSuaTaiKhoan;
+    private ImageView imgAvatar;
+    private TextView tvName,tvEmail;
+    private Button btnLogOut;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View  view= (View) inflater.inflate(R.layout.fragment_profile, container, false);
-        tvChinhSuaTaiKhoan = (TextView) view.findViewById(R.id.tv_chinhsuataikhoan);
-        tvChinhSuaTaiKhoan.setOnClickListener(new View.OnClickListener() {
+        View view= (View) inflater.inflate(R.layout.fragment_profile, container, false);
+        layoutChinhSuaTaiKhoan = (LinearLayout) view.findViewById(R.id.layout_chinhsuataikhoan);
+        tvName = (TextView) view.findViewById(R.id.tv_name);
+        tvEmail = (TextView) view.findViewById(R.id.tv_email);
+        imgAvatar = (ImageView) view.findViewById(R.id.imgAvatar);
+        btnLogOut = (Button) view.findViewById(R.id.btn_Logout);
+        showUserInformation(view);
+        layoutChinhSuaTaiKhoan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent= new Intent(getActivity(),ProfileUpdateActivity.class);
                 getActivity().startActivity(intent);
+                getActivity().finish();
             }
         });
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent= new Intent(getActivity(),LoginActivity.class);
+                getActivity().startActivity(intent);
+            }
+        });
+
         return view;
+    }
+    private void showUserInformation(View view){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user == null){
+            return;
+        }
+        String strName = user.getDisplayName();
+        String strEmail = user.getEmail();
+        Uri photoUrl = user.getPhotoUrl();
+        tvName.setText(strName);
+        tvEmail.setText(strEmail);
+//        Glide.with(getActivity()).load(photoUrl).error(R.drawable.avatar_default).into(imgAvatar);
     }
 }
